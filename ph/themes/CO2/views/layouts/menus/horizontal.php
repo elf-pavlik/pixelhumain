1f59b79a5d4ce70c8895296dcb4ccce902c36eef
Bouboule <clement.damiens@gmail.com>
2018-12-18 17:30:42 +0100

    fix horizontal view

--- ph/themes/CO2/views/layouts/menus/horizontal.php
+++ ph/themes/CO2/views/layouts/menus/horizontal.php
@@ -1,4 +1,6 @@
-<?php $visibleClass=(@$params["numberOfApp"]<=1) ? "visible-xs": ""; ?>
+<?php $visibleClass=(@$params["numberOfApp"]<=1) ? "visible-xs": ""; 
+if(@$useFilter && !empty($useFilter)){
+?>
 <div id="territorial-menu" class="col-md-12 col-sm-12 col-xs-12 margin-bottom-10 <?php echo $visibleClass ?>">
     <button class="btn visible-xs pull-left menu-btn-scope-filter text-red elipsis"
         data-type="<?php echo @$type; ?>">
@@ -18,3 +20,4 @@
         } ?>
         <button class="btn btn-show-filters"><?php echo Yii::t("common", "Filters") ?> <span class="topbar-badge badge animated bounceIn badge-warning bg-green"></span> <i class="fa fa-angle-down"></i></button>
 </div>
+<?php } ?>
